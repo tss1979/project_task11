8763a597130d80c8e74b0515960206fde6c885a6
sergeytashkinov <tashkinov2@gmail.com>
2024-05-16 10:29:35 +0300

    fixed conflict

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def str_upper(word):
+    """Функция принимает на вход строку и возвращает ее со всеми заглавными буквами"""
     return word.upper()
+
+def words_title(words_str):
+    """Функция делает заглавными первые буквы каждого слова в строке,
+       поступившей на вход функции."""
+    return ' '.join([word.title() for word in words_str.split()])
